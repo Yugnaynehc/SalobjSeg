2d5ceb518849500a5f3f3e022c62ce9d775afd4e
chenyangyu <chenyangyu@bytedance.com>
2018-04-12 22:57:36 +0800

    init

--- train.py
+++ train.py
@@ -6,13 +6,15 @@ Training code
 from __future__ import print_function
 from __future__ import absolute_import
 
+import os
 import torch
 from torch.autograd import Variable
+from torchvision.utils import save_image
 from tensorboard_logger import configure, log_value
 
 from data import get_loader
 from model import Model
-from args import image_root, gt_root
+from args import image_root, gt_root, visual_dir
 from args import opt, log_environment
 from utils import clip_gradient, adjust_lr
 
@@ -53,8 +55,11 @@ for epoch in range(1, opt.epoch + 1):
         loss = crit(res, gts)
         # Backward and update
         loss.backward()
-        clip_gradient(optimizer, clip)
+        clip_gradient(optimizer, opt.clip)
         optimizer.step()
+        log_value('loss', loss.data[0], (epoch - 1) * total_step + i)
         if i % 10 == 0 or i == total_step:
             print('Epoch [%d/%d], Step [%d/%d], Loss: %.8f' %
                   (opt.epoch, opt.epoch, i, total_step, loss.data[0]))
+            save_image(images.data, os.path.join(visual_dir, 'images_%d_%d.png' % (epoch, i)))
+            save_image(gts.data, os.path.join(visual_dir, 'gts_%d_%d.png' % (epoch, i)))
